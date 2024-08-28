1f2da23524135160816a7633ec9eaa207810442d
1Dmarc1 <97765697+1Dmarc1@users.noreply.github.com>
2024-08-28 19:57:47 +0200

    set correct version

--- src/main/java/me/dmarc/compasstracker/CompassUpdateTask.java
+++ src/main/java/me/dmarc/compasstracker/CompassUpdateTask.java
@@ -8,6 +8,7 @@ import org.bukkit.GameMode;
 import org.bukkit.Location;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.Plugin;
+import org.bukkit.potion.PotionEffect;
 
 import java.util.Collection;
 import java.util.HashMap;
